360c7e2d01ec5f62e918f9d059f8ab791257ef5e
coni <conijocus@gmail.com>
2025-10-15 16:51:42 +0200

    ejem8
    Simular colisiones en el espacio

---
+++ 2D/ejem8(Simular colision en el espacio)/Assets/scripts/otras_colisiones.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class otras_colisiones : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Se produce una colision");
+    }
+
+}
